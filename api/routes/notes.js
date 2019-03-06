cd24a1befc36694e9a1b126ca812baf08312e71e
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-03-07 00:35:47 +0100

    Added a controller for users related endpoints

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -4,16 +4,10 @@ const router = express.Router();
 const NotesController = require('../controllers/notes');
 
 router.get('/', NotesController.get_all_notes);
-
 router.post('/', NotesController.post_note);
-
 router.get('/user/:id', NotesController.get_one_user_notes);
-
 router.get('/:id', NotesController.get_one_note);
-
 router.patch('/:id', NotesController.edit_one_note);
-
 router.delete('/:id', NotesController.delete_one_note);
-
 
 module.exports = router;
