ce169467cd95c8c1aa6e229c1ecb1bb9fa09b01f
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-03-09 19:06:49 +0100

    commented server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+//Create the Server!
+
 const http = require("http");
+//Insert PORT here or define in nodemon.json
 const port = process.env.SERVER_PORT || 3000;
 const app = require('./app');
 const server = http.createServer(app);
